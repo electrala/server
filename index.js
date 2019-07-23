329943c6b5f3bf46fb90ad50665d0b01fd9112be
Ayana Hawk <ayanahawk11@gmail.com>
2019-07-23 15:50:53 -0700

    adding errors to insert method

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const Snippet = require('./models/Snippet.model');
 
 async function testModels() {
-  const snippets = await Snippet.select({
-    language: 'javascript',
-    author: 'Scott',
-  });
-  console.log(snippets);
+  //   const snippets = await Snippet.select({
+  //     language: 'javascript',
+  //     author: 'Scott',
+  //   });
+  //   console.log(snippets);
+
+  try {
+    await Snippet.insert({
+      author: 'Ayana',
+      code: 'code, code, code',
+      title: 'Ayana is the best',
+      description: 'Ayana got is ssss',
+      language: 'javacript',
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 testModels();
--- models/Snippet.model.js
+++ models/Snippet.model.js
@@ -1,13 +1,16 @@
 const fs = require('fs').promises;
 const path = require('path');
+const shortid = require('shortid');
 
 /**
  * @typedef {Object} Snippet
  * @property {sring} id
  * @property {string} author
  * @property {string} code
- * @property {sring} id
+ * @property {sring} title
  * @property {string} author
+ * @property {string} description
+ * @property {string} language
  * @property {string} comments
  * @property {number} favorites
  */
@@ -30,6 +33,33 @@ exports.select = async (query = {}) => {
     return filtered;
   } catch (err) {
     console.log('ERROR in Snippet');
+    throw err;
+  }
+};
+
+// CREATE
+
+exports.insert = async ({ author, code, title, description, language }) => {
+  try {
+    const dbpath = path.join(__dirname, '..', 'db', 'snippets.json');
+    const snippets = JSON.parse(await fs.readFile(dbpath));
+    // read snippets.json
+    // grab data
+    // parse datat
+    // write to the file
+    snippets.push({
+      id: shortid.generate(),
+      author,
+      code,
+      title,
+      description,
+      language,
+      comments: [],
+      favorites: 0,
+    });
+    return fs.writeFile(dbpath, JSON.stringify(snippets));
+  } catch (err) {
+    console.log(err, 'ERROR from snippets');
     throw err;
   }
 };
