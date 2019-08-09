5eec351597a46fcf1f29e5c433d079560632a221
CJ Nunez <cjnunez93@yahoo.com>
2019-08-09 09:31:18 -0500

    documentation for all my files

--- controllers/registration.controller.js
+++ controllers/registration.controller.js
@@ -1,8 +1,12 @@
 const userRegistration = require('../models/userRegistration.model');
 const ErrorStatus = require('../utils/ErrHTTP');
 
+// CREATES USER
 exports.createUser = async (request, response) => {
+  // sending a request to server
+  // user registration is grabbing the path
   const user = await userRegistration.insert(request.body);
+  // sending the response back to the user
   response.status(201).send(user);
   console.log(request.body, 'recieved');
 };
@@ -13,10 +17,14 @@ exports.getUsers = async ({ query }, response) => {
   return response.send(users);
 };
 
+// GETS USER BY THEIR ID
 exports.getUserById = async (request, response) => {
   try {
+    // will grab the user by their ID
     const { id } = request.params;
     const users = await userRegistration.select({ id });
+
+    // checking if the user lenght is zero if so will throw an error
     if (users.length === 0) {
       throw new Error('id does not exist', 404);
     }
@@ -26,14 +34,18 @@ exports.getUserById = async (request, response) => {
   }
 };
 
+// DELETE user by ID
 exports.deleteUser = async (request, response) => {
+  // checking the user of ID
   const { id } = request.params;
   await userRegistration.delete(id);
 
-  // Add error to delete when the id isnt there anymore
+  // TODO: Add error to delete when the id isnt there anymore
   response.send(`deleted id: ${id}`);
 };
 
+// UPDATE user by their ID
+// TODO: add try catch to see if the ID is real
 exports.updateUser = async (request, response) => {
   // need to bring in body as an object
   const data = request.body;
