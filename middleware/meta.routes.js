bc6d0cc7841ca8ca9930218a688690587c62d7e2
Brennan Newton <brennanmnewton@gmail.com>
2019-08-13 09:57:02 -0700

    Meta route

---
+++ middleware/meta.routes.js
@@ -0,0 +1,6 @@
+const registrationRoute = require('./registration.routes');
+const critiquesRoute = require('./critiques.routes');
+
+const routeObj = { registrationRoute, critiquesRoute };
+
+module.exports = routeObj;
