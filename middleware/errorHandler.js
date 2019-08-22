6802e0dd49d21e35c8111acbe291d95ae7925f04
Dandy <dandyjreyes@gmail.com>
2019-08-22 10:42:53 -0700

    Error handling, commenting code

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,12 @@
 const ErrHTTP = require('../utils/ErrHTTP');
 
+/**
+ * Handle errors with server
+ * @param {object} err
+ * @param {object} request
+ * @param {object} response
+ * @param {object} next
+ */
 const errorHandler = (err, request, response, next) => {
   if (err instanceof ErrHTTP) response.status(err.status).send(err.message);
   else response.status(500).send('Server error');
--- utils/db.utils.js
+++ utils/db.utils.js
@@ -1,13 +1,23 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+/**
+ * Read file from database
+ * @param {object} fileName
+ * @returns {object} A parsed JSON file of the passed file
+ */
 exports.readJsonFromDb = async fileName => {
   const dbpath = path.join(__dirname, '..', 'db', `${fileName}.json`);
   return JSON.parse(await fs.readFile(dbpath));
 };
 
+/**
+ * Writes to a JSON file
+ * @param {object} fileName
+ * @param {object} data
+ * @returns {object} stringified JSON object
+ */
 exports.writeJsonToDb = async (fileName, data) => {
-  // where the file to
   const dbpath = path.join(__dirname, '..', 'db', `${fileName}.json`);
   return fs.writeFile(dbpath, JSON.stringify(data));
 };
