483e93b89f0ab1068e24f8e53227bfc2608f40e1
CJ Nunez <cjnunez93@yahoo.com>
2019-09-11 11:37:06 -0500

    sql update

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,4 @@
-DROP TABLE IF EXISTS critiques;
 
+CREATE TABLE critiques (id SERIAL PRIMARY KEY,
+                                          username TEXT, title TEXT, description TEXT, questions TEXT, genre TEXT, comments TEXT[]);
 
-CREATE TABLE critiques(id SERIAL PRIMARY KEY,
-                                         username TEXT, title TEXT, description TEXT, questions TEXT, genre TEXT, comments TEXT[]);
-
-
-INSERT INTO critiques (title, description, questions, genre, comments)
-VALUES ('Ye who yeeeeeeets',
-        'Son in this life you either yeet or get yeeted!',
-        'Does this quote truly capture a generation?',
-        'vilinry',
-        '{"Inserting a fucking array is a nightmare!"}');
