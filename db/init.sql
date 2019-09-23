2d5d9a6187f373d45a87f76c8fd16a78e1d7bf8c
Dandy <dandyjreyes@gmail.com>
2019-09-23 09:10:33 -0700

    Connected user login to backend as well as authentication

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,22 @@
-CREATE TABLE critiques(id SERIAL PRIMARY KEY, username TEXT, title TEXT, description TEXT, questions TEXT, genre TEXT, comments TEXT[]);
+DROP TABLE IF EXISTS critiques;
+DROP TABLE IF EXISTS users;
+CREATE TABLE critiques (
+  id SERIAL PRIMARY KEY,
+  username TEXT,
+  title TEXT,
+  description TEXT,
+  questions TEXT,
+  genre TEXT,
+  comments TEXT []
+);
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firstName TEXT,
+  lastName TEXT,
+  email TEXT,
+  userName TEXT,
+  password TEXT,
+  confirmPassword TEXT,
+  pronoun TEXT,
+  location TEXT
+)
