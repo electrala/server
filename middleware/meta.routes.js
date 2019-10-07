abe1fe7951f22838f87f2accb50d26c4ef3abd37
Brennan Newton <brennanmnewton@gmail.com>
2019-10-07 10:34:49 -0700

    log in route

--- middleware/meta.routes.js
+++ middleware/meta.routes.js
@@ -6,7 +6,5 @@ const app = express();
 
 app.use('/users', usersRouter);
 app.use('/critiques', critiquesRouter);
-app.use('/', (request, response) => {
-  response.send('your mom!');
-});
+
 module.exports = app;
