4115b1ae06737468dca6eff115139929721fdc6c
Adam Oliveras <theadamoliveras@gmail.com>
2019-10-17 14:33:44 -0700

    minor modification.

--- middleware/meta.routes.js
+++ middleware/meta.routes.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const usersRouter = require('./users.routes');
-const critiquesRouter = require('./critiques.routes');
+const express = require("express");
+const usersRouter = require("./users.routes");
+const critiquesRouter = require("./critiques.routes");
 
 const app = express();
 
-app.use('/users', usersRouter);
-app.use('/critiques', critiquesRouter);
+app.use("/users", usersRouter);
+app.use("/critiques", critiquesRouter);
 
 module.exports = app;
