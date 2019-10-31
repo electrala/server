48fc896bdcd85a40ddf0021b0187c162e2b8af55
Brennan Newton <brennanmnewton@gmail.com>
2019-10-31 09:24:46 -0700

    Added test message for heroku debugging

--- middleware/meta.routes.js
+++ middleware/meta.routes.js
@@ -5,6 +5,10 @@ const awsRouter = require('./aws.routes');
 
 const app = express();
 
+app.use('/', (request, response) => {
+    // console.log("We're in the router");
+    response.send('Welcome to Snips!');
+  });
 app.use('/users', usersRouter);
 app.use('/critiques', critiquesRouter);
 app.use('/aws', awsRouter);
