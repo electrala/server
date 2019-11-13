6b373ecd6fd1763486b56b08df2275299f666c46
Adam Oliveras <theadamoliveras@gmail.com>
2019-11-13 15:59:40 -0800

    Added expiration logic.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -55,7 +55,7 @@ exports.logIn = async (request, response, next) => {
     const isMatch = await bcrypt.compare(request.body.password, user.password);
     if (!isMatch) throw new ErrHTTP("This password doesn't match", 401);
     // We can place the whole object where the user.username if we needed to
-    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "12h" });
     response.send({ message: 'Logged in!', token });
   } catch (err) {
     next(err);
