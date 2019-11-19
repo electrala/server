9429929a4012ecea0b12b8e1d60ddd738b3e0232
Adam Oliveras <theadamoliveras@gmail.com>
2019-11-19 11:11:26 -0800

    Created comments router.

--- middleware/aws.routes.js
+++ middleware/aws.routes.js
@@ -81,7 +81,6 @@ router.post('/critique-img-upload', (req, res) => {
         // If Success
         const imageName = req.file.key;
         const imageLocation = req.file.location;
-        // Save the file name into database into profile model
         res.json({
           image: imageName,
           location: imageLocation,
---
+++ middleware/comments.routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const critiques = require('../controllers/comments.controller');
+const validate = require('./validate');
+
+const router = express.Router();
+
+// Gets all comments in table
+router.get('/all', comments.getAllComments);
+
+// Adds a new comment to the table
+router.post('/new', validate, comments.createComment);
+
+// Select a single critique by its ID
+router.get('/:id', comments.getCommentByID);
+
+// Select comments related to a single critique it belongs to
+router.get('/:critiqueID', comments.getCommentsBelongingToCritique);
+
+// Select comments related to a single user
+router.get('/:username', comments.getCommentsBelongingToUser);
+
+// Updates a comment
+router.patch('/:id', validate, comments.updateComment);
+
+// Deletes a comment by id
+router.delete('/:id', validate, comments.deleteComment);
+
+
+module.exports = router;
